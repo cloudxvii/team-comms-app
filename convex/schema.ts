950aa3bf9e4a3be8dd29b5b2ae4511464e13d6bc
CLoudXVII <sharkov.vs@outlook.com>
2026-04-05 21:11:27 +0300

    feat: added workspace get query

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,8 +1,15 @@
-import { defineSchema } from "convex/server";
+import { v } from "convex/values";
+import { defineSchema, defineTable } from "convex/server";
+
 import { authTables } from "@convex-dev/auth/server";
 
 const schema = defineSchema({
   ...authTables,
+  workspaces: defineTable({
+    name : v.string(),
+    iserId: v.id("users"),
+    joinCode: v.string(),
+  }),
 });
 
 export default schema;
